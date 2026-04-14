dc35bd2930d97a71dddb6377627e1d25886bb0ad
anjing2269 <244328606+anjing2269@users.noreply.github.com>
2026-04-14 19:45:20 +0900

    キーボードでメッセージを投稿できる機能を追加

--- public/nn-chat.js
+++ public/nn-chat.js
@@ -3,3 +3,29 @@
 window.addEventListener('load', () => {
   window.scrollTo(0, document.body.scrollHeight);
 });
+
+// Enterキー と Ctrlキー(MacはCmdキー) を押していたら送信
+const formElement = document.forms['message-form'];
+const textareaElement = formElement.elements['content'];
+textareaElement.addEventListener('keydown', (event) => {
+  // 送信キーを押したら
+  if (isPressedSubmitKey(event)) {
+    // キーボード入力をキャンセルして送信
+    event.preventDefault();
+    formElement.submit();
+  }
+});
+
+// 送信キーを押しているかを判定
+function isPressedSubmitKey(event) {
+  if (event.key !== 'Enter') {
+    return false;
+  }
+  if (event.ctrlKey) {
+    return true;
+  }
+  // MacのCmdキーはmetaKeyという名前
+  if (event.metaKey) {
+    return true;
+  }
+}
